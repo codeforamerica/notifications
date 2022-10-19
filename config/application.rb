7b63939670865073c54820e370d07e5ecabfde71
Vraj Mohan <vraj@codeforamerica.org>
2022-10-18 19:58:51 -0700

    Store Twilio configuration and secrets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Notifications
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.twilio = config_for(:twilio)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
