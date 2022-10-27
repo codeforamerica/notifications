8e97658d0f16d2f8b353ef97e8d014c7b77ab3fd
Mike Rotondo <mrotondo@gmail.com>
2022-10-26 17:38:03 -0700

    Switch to using an ENV-provided ngrok url for status callbacks. This should be added to a new config/local_env.yml file which is gitignored.

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -4,7 +4,7 @@ class MessageService
   def send_message(to, body)
     message = twilio_client.messages.create(
       from: twilio_number,
-      status_callback: "https://0675-2601-648-8403-9d00-d0fb-7125-e350-4319.ngrok.io/sms/status_callback",
+      status_callback: "#{ENV['NGROK_URL']}/sms/status_callback",
       to: to,
       body: body
     )
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,13 @@ module Notifications
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.fallbacks = true
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end
