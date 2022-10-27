0e593df4d1ecfba178d09a0df61e416b634b0ae2
Vraj Mohan <vraj@codeforamerica.org>
2022-10-27 14:57:47 -0700

    Update nullability on Recipient columms

--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -3,12 +3,12 @@
 # Table name: recipients
 #
 #  id               :bigint           not null, primary key
-#  phone_number     :string
-#  program          :string
+#  phone_number     :string           not null
+#  program          :string           not null
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  message_batch_id :bigint           not null
-#  program_case_id  :string
+#  program_case_id  :string           not null
 #
 # Indexes
 #
