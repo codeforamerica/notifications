73f0c924a9bce5d275fbca90bc58985ee22fdb02
Vraj Mohan <vraj@codeforamerica.org>
2022-10-26 18:45:19 -0700

    Add tests for MessageTemplate

--- spec/models/message_template_spec.rb
+++ spec/models/message_template_spec.rb
@@ -15,5 +15,17 @@
 require 'rails_helper'
 
 RSpec.describe MessageTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "stores a body in different locales" do
+    message_template = MessageTemplate.new(name: 'Message1', body: 'Message body in English')
+    Mobility.with_locale(:es) { message_template.body = 'Cuerpo del mensaje en español' }
+    expect(message_template.body).to eq('Message body in English')
+    Mobility.with_locale(:es) { expect(message_template.body).to eq('Cuerpo del mensaje en español') }
+  end
+
+  context "when no translation is available for a supported locale" do
+    it "defaults to the default locale" do
+      message_template = MessageTemplate.new(name: 'Message1', body: 'Message body')
+      Mobility.with_locale(:es) { expect(message_template.body).to eq('Message body') }
+    end
+  end
 end
