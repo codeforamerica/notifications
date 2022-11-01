da7b13b8faa10268c9a3912d2c661dd4d474355d
Vraj Mohan <vraj@codeforamerica.org>
2022-11-01 16:07:07 -0700

    Consolidate commands

--- cli/notifications_cli.rb
+++ cli/notifications_cli.rb
@@ -11,4 +11,9 @@ class NotificationsCli < Thor
   rescue Exception => e
     puts e.message
   end
+
+  desc "send_test_message to_number message_body", "Sends a single SMS containing message_body to to_number. e.g. thor message:send_test_message +15555555555 'Hello, this is phone'"
+  def send_test_message(to_number, message_body)
+    MessageService.new.send_message(to_number, message_body)
+  end
 end
