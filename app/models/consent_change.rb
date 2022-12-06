18a7cd107dc0c4d12e5399fba6989ec2b393302c
Mike Rotondo <mrotondo@gmail.com>
2022-12-06 12:37:21 -0800

    consent change scope cleaup

--- app/models/consent_change.rb
+++ app/models/consent_change.rb
@@ -23,11 +23,13 @@ class ConsentChange < ApplicationRecord
   belongs_to :sms_message
   belongs_to :program
 
-  def self.latest_for(phone_number, program)
-      joins(:sms_message)
+  scope :for_number_and_program, ->(phone_number, program) {
+    joins(:sms_message)
       .where(sms_message: { from: phone_number },
              program: program)
-      .order(:created_at)
-      .last
+  }
+
+  def self.latest(phone_number, program)
+    for_number_and_program(phone_number, program).order(:created_at).last
   end
 end
--- app/services/consent_service.rb
+++ app/services/consent_service.rb
@@ -17,7 +17,7 @@ class ConsentService
 
   def check_consent(recipient, program)
     default_consent = true
-    latest_consent_change = ConsentChange.latest_for(recipient.phone_number, program)
+    latest_consent_change = ConsentChange.latest(recipient.phone_number, program)
     if latest_consent_change.present?
       latest_consent_change.new_consent
     else
