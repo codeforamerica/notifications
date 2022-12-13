d159efdc9a00053d2d038d284cda6e6b4dbeaf16
Mike Rotondo <mrotondo@gmail.com>
2022-12-13 15:59:31 -0800

    attempt 2 to allow aptible to run this migration

--- db/migrate/20221213010258_add_help_to_program.rb
+++ db/migrate/20221213010258_add_help_to_program.rb
@@ -1,8 +1,8 @@
 class AddHelpToProgram < ActiveRecord::Migration[7.0]
   def change
     change_table :programs do |t|
-      t.column :help_keywords, :jsonb, null: false, default: []
-      t.column :help_response, :jsonb, null: false, default: {}
+      t.column :help_keywords, :jsonb
+      t.column :help_response, :jsonb, default: {}
     end
   end
 end
