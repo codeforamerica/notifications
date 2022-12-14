e5c53e38b8a087588ea97ba6734f7ffe23430069
Mike Rotondo <mrotondo@codeforamerica.org>
2022-12-13 16:56:15 -0800

    add default value to help keyword migration - REQUIRES DB RE-MIGRATE IF YOU ALREADY RAN THIS MIGRATION (#23)

--- db/migrate/20221213010258_add_help_to_program.rb
+++ db/migrate/20221213010258_add_help_to_program.rb
@@ -1,7 +1,7 @@
 class AddHelpToProgram < ActiveRecord::Migration[7.0]
   def change
     change_table :programs do |t|
-      t.column :help_keywords, :jsonb, null: false
+      t.column :help_keywords, :jsonb, null: false, default: []
       t.column :help_response, :jsonb, null: false, default: {}
     end
   end
