700e337f0fee10c34de0e4894566fd88fef35266
Mike Rotondo <mrotondo@gmail.com>
2023-01-10 16:44:17 -0800

    Add final keyword responses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# announcements = Program.create!(
-#   name: "ANNOUNCEMENTS",
-#   opt_in_keywords: {en: ["STARTANNOUNCEMENTS"], es: ["STARTANNOUNCEMENTSSPANISH"]},
-#   opt_out_keywords: {en: ["STOPANNOUNCEMENTS"], es: ["ALTOANNOUNCEMENTS"]},
-#   opt_in_response: "Hello",
-#   opt_out_response: "Goodbye"
-# )
-# Mobility.with_locale(:es) do
-#   announcements.opt_in_response = "Hola"
-#   announcements.opt_out_response = "Adios"
-# end
-# announcements.save
 
 snap = Program.new(
   name: "SNAP",
@@ -32,13 +14,13 @@ snap = Program.new(
     es: %w(AYUDA) },
 )
 Mobility.with_locale(:en) do
-  snap.opt_in_response = "Thanks for joining DSS reminders! You will receive 5 messages per month. Message/data rates may apply. Reply HELP for help, STOP to cancel."
-  snap.opt_out_response = "You have successfully unsubscribed from DSS reminders. You will not receive any more messages from this number. Reply START to resubscribe."
-  snap.help_response = "CT DSS messages. Get help by logging into mydss.ct.gov or calling 855-626-6632. Five messages per month. Message/data rates may apply. Reply STOP to cancel."
+  snap.opt_in_response = "You subscribed to CT DSS SNAP messages. We'll send you less than 5 texts each month to help you get and keep your SNAP benefits. Message/data rates may apply. Reply HELP for help, OptOutSNAP to cancel."
+  snap.opt_out_response = "You have unsubscribed from CT DSS SNAP messages. You will not get any more texts about your SNAP benefits. Reply StartSNAP to resubscribe."
+  snap.help_response = "CT DSS messages. Get help by logging into mydss.ct.gov or calling 855-626-6632. Message/data rates may apply. Reply STOP to cancel."
 end
 Mobility.with_locale(:es) do
-  snap.opt_in_response = "Gracias por inscribirse a recibir los recordatorios de DSS! Tarifas de mensajes y datos pueden aplicar. Responda AYUDA para recibir ayuda, o DETENER para cancelar."
-  snap.opt_out_response = "Se ha dado de baja de recibir recordatorios de DSS. No recibirá más mensajes de este número. Responda COMENZAR para reinscribirse."
-  snap.help_response = "Mensajes de CT DSS. Para ayuda llamando al 1-855-626-6632. 5 mensajes al mes. Tarifas de mensajes y datos pueden aplicar. Responda ALTO para cancelar."
+  snap.opt_in_response = "Se suscribió a los mensajes de CT DSS SNAP. Le enviaremos menos de 5 mensajes de texto cada mes para ayudarlo a obtener y mantener sus beneficios de SNAP. Se pueden aplicar tarifas de mensajes/datos. Responda HELP para obtener ayuda, OptOutSNAP para cancelar."
+  snap.opt_out_response = "Se ha dado de baja de los mensajes de CT DSS SNAP. No recibirá más mensajes de texto sobre sus beneficios de SNAP. Responda StartSNAP para volver a suscribirse."
+  snap.help_response = "Mensajes de CT DSS. Obtenga ayuda iniciando sesión en mydss.ct.gov o llamando 855-626-6632. Se pueden aplicar tarifas de mensajes/datos. Responda STOP para cancelar."
 end
 snap.save!
